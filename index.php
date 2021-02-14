32b71076c88fcce54567e8fb41cae7e373549ad6
amawrii <amawrii@gmail.com>
2021-02-14 03:07:09 -0300

    adicionando index.php

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+    <meta charset= "UTF-8">
+        <link rel="stylesheet" src="estilo.css">
+         <title>#Desafio100Vídeos</title>
+    </head>
+    
+    <style>
+        .cabecalho {
+            display: flex;
+            justify-content: space-around;
+        }
+        .conteudo {
+            display: flex;
+            justify-content: space-around;
+        }
+        .item {
+            /* O flex: 1; é necessário para que cada item se expanda ocupando o tamanho máximo do container. */
+            flex: 1;
+            margin: 5px;
+            text-align: center;
+        }
+        .wrap {
+            flex-wrap: wrap;
+        }
+    </style>
+
+    <body> 
+           
+            <div class="cabecalho">
+                <h1>Desafio100Vídeos</h1>
+                <h2>Faltam 100 vídeos</h2>
+            </div>
+
+            <div class="conteudo wrap">
+                <img src="images/img00.jpg" class="item "/>
+                <img src="images/img00.jpg" class="item "/>
+                <img src="images/img00.jpg" class="item "/>
+                <img src="images/img00.jpg" class="item "/>
+                <img src="images/img00.jpg" class="item "/>
+                <img src="images/img00.jpg" class="item "/>
+                <img src="images/img00.jpg" class="item "/>
+                <img src="images/img00.jpg" class="item "/>
+                <img src="images/img00.jpg" class="item "/>
+                <img src="images/img00.jpg" class="item "/>
+                <img src="images/img00.jpg" class="item "/>
+                <img src="images/img00.jpg" class="item "/>
+                <img src="images/img00.jpg" class="item "/>
+                <img src="images/img00.jpg" class="item "/>
+                <img src="images/img00.jpg" class="item "/>    
+            </div> 
+
+    </body>
+  </html>
